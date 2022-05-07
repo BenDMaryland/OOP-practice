6b8452219a23dcb493c627b9cc8d3429febc2bf4
My Name <ben.d.maryland@gmail.com>
2022-05-06 21:24:01 -0400

    basic object

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world")
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 2
+    },
+    draw: function(){
+        console.log("hi",this)
+    }
+}
+
+circle.draw()
