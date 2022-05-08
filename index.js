b0e84644a2173f6d0a11a4552506e7f4a0e53f87
My Name <ben.d.maryland@gmail.com>
2022-05-07 21:20:44 -0400

    Now cats not OOP

--- index.js
+++ index.js
@@ -1,27 +1,70 @@
 
-class Circle {
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed');
 
-    constructor(radius) {
-        this.radius = radius
 
-        this.location = {
-            x: 1,
-            y: 2
-        },
-            this.draw = function () {
-                console.log("hi", this)
-            }
-    }
+
+let url = "https://api.thecatapi.com/v1/breeds?limit=10"
+let container = document.querySelector(".catContainer")
+container.innerText= "hello"
+
+const fetchSongs = async () => {
+    const response = await fetch(url)
+    const cats = await response.json();
+ 
+     catDisplayer(cats) 
 }
 
-const reg = new Circle(2)
+fetchSongs()
 
-function adder(){
 
-this.radius++
+
+
+function catDisplayer(cats){
+
+
+cats.forEach(cat => {
+    console.log(cat, cat.name)
+  
+    let card = document.createElement('div')
+    let title = document.createElement("h1")
+    let image = document.createElement('img')
+    image.src= cat.image.url
+    title.innerText = cat.name
+console.log(image)
+    container.appendChild(card)
+    card.appendChild(title)
+    card.appendChild(image)
+})
 
 }
+// class Circle {
 
-console.log(reg)
-adder.call(reg)
-console.log(reg)
+//     constructor(radius) {
+//         this.radius = radius
+
+//         this.location = {
+//             x: 1,
+//             y: 2
+//         },
+//             this.draw = function () {
+//                 console.log("hi", this)
+//             }
+//     }
+// }
+
+// const reg = new Circle(2)
+
+// function adder() {
+
+//     this.radius++
+
+// }
+
+// console.log(reg)
+// adder.call(reg)
+// console.log(reg)
+
+// // 
+
+});
