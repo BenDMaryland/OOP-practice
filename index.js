95d52a63e8f76a06662fec2c3274a144d90e4bc4
My Name <ben.d.maryland@gmail.com>
2022-05-07 23:19:42 -0400

    Now cats not OOP

--- index.js
+++ index.js
@@ -1,72 +1,88 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
+    let url = "https://api.thecatapi.com/v1/breeds"
+    let container = document.querySelector(".catContainer")
+    class Cat {
+
+        constructor(cat) {
+            this.cat = cat
+
+            this.makeACat = () => {
+                console.log(this)
+                let card = document.createElement('div')
+                let title = document.createElement("h1")
+                let image = document.createElement('img')
+                let desc = document.createElement('h3')
+                container.appendChild(card).className = "card"
+                card.appendChild(title).innerText = this.cat.name
+                card.appendChild(image).src = this.cat.image.url
+                card.appendChild(desc).innerText = this.cat.description
+
+            }
+
+        }
 
 
 
-let url = "https://api.thecatapi.com/v1/breeds?limit=10"
-let container = document.querySelector(".catContainer")
 
-
-const fetchSongs = async () => {
-    const response = await fetch(url)
-    const cats = await response.json();
- 
-     catDisplayer(cats) 
-}
-
-fetchSongs()
+    }
 
 
 
 
-function catDisplayer(cats){
+    const fetchSongs = async () => {
+        const response = await fetch(url)
+        const cats = await response.json();
+
+        catDisplayer(cats)
+    }
+
+    fetchSongs()
 
 
-cats.forEach(cat => {
-    console.log(cat, cat.name)
-  
-    let card = document.createElement('div')
-    let title = document.createElement("h1")
-    let image = document.createElement('img')
-    
-    card.className="card"
-    image.src= cat.image.url
-    title.innerText = cat.name
-console.log(image)
-    container.appendChild(card)
-    card.appendChild(title)
-    card.appendChild(image)
-})
 
-}
-// class Circle {
 
-//     constructor(radius) {
-//         this.radius = radius
+    function catDisplayer(cats) {
 
-//         this.location = {
-//             x: 1,
-//             y: 2
-//         },
-//             this.draw = function () {
-//                 console.log("hi", this)
-//             }
-//     }
-// }
 
-// const reg = new Circle(2)
+        cats.forEach(cat => {
+            if (cat.image) {
+                const catCard = new Cat(cat)
+                catCard.makeACat()
+            }
 
-// function adder() {
 
-//     this.radius++
+        })
 
-// }
+    }
+    // class Circle {
 
-// console.log(reg)
-// adder.call(reg)
-// console.log(reg)
+    //     constructor(radius) {
+    //         this.radius = radius
 
-// // 
+    //         this.location = {
+    //             x: 1,
+    //             y: 2
+    //         },
+    //             this.draw = function () {
+    //                 console.log("hi", this)
+    //             }
+    //     }
+    // }
+
+    // const reg = new Circle(2)
+
+    // function adder() {
+
+    //     this.radius++
+
+    // }
+
+    // console.log(reg)
+    // adder.call(reg)
+    // console.log(reg)
+
+    // // 
 
 });
