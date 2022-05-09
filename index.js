9e4eeed3ce165232a272cf04b83e5ef8399a929f
My Name <ben.d.maryland@gmail.com>
2022-05-08 23:25:03 -0400

    Vanillia JS sucks

--- index.js
+++ index.js
@@ -1,88 +1,68 @@
 
 window.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM fully loaded and parsed');
     let url = "https://api.thecatapi.com/v1/breeds"
     let container = document.querySelector(".catContainer")
+    let search = document.querySelector(".search")
+    let searchBar = document.querySelector(".searchBar")
+    let allCats
+    let filteredCats=[]
+    search.addEventListener("keypress", searchHandler)
+
+    fetch(url)
+        .then(r => r.json())
+        .then(data => allCats = data)
+
+    function searchHandler(e) {
+        searchBar.innerHTML = ``
+      filteredCats = allCats.filter(cat =>  cat.name.includes(e.target.value)  )
+ filteredCats.map(cat=>{
+     let desc = document.createElement('h3')
+     searchBar.appendChild(desc).innerText=cat.name
+
+ })
+      
+
+    }
+
+
     class Cat {
-
-        constructor(cat) {
+        constructor(cat,location) {
             this.cat = cat
-
             this.makeACat = () => {
-                console.log(this)
                 let card = document.createElement('div')
                 let title = document.createElement("h1")
                 let image = document.createElement('img')
                 let desc = document.createElement('h3')
-                container.appendChild(card).className = "card"
+                container.appendChild(card).className = location
                 card.appendChild(title).innerText = this.cat.name
                 card.appendChild(image).src = this.cat.image.url
                 card.appendChild(desc).innerText = this.cat.description
 
             }
-
         }
-
-
-
-
     }
-
 
 
 
     const fetchSongs = async () => {
         const response = await fetch(url)
         const cats = await response.json();
-
         catDisplayer(cats)
+        return await cats
     }
-
     fetchSongs()
 
-
-
-
     function catDisplayer(cats) {
-
-
         cats.forEach(cat => {
             if (cat.image) {
-                const catCard = new Cat(cat)
-                catCard.makeACat()
+                if (cat.image.url) {
+
+                    const catCard = new Cat(cat,"card")
+                    catCard.makeACat()
+                }
             }
-
-
         })
 
     }
-    // class Circle {
-
-    //     constructor(radius) {
-    //         this.radius = radius
-
-    //         this.location = {
-    //             x: 1,
-    //             y: 2
-    //         },
-    //             this.draw = function () {
-    //                 console.log("hi", this)
-    //             }
-    //     }
-    // }
-
-    // const reg = new Circle(2)
-
-    // function adder() {
-
-    //     this.radius++
-
-    // }
-
-    // console.log(reg)
-    // adder.call(reg)
-    // console.log(reg)
-
-    // // 
 
 });
